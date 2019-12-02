bayandin/chromedriver

commit c9f2cbb6951dc8c2d96dd78a0c531f14660df324

events: ozone: Remove implicit init from GetKeyboardLayoutEngine
GetKeyboardLayoutEngine() has the side effect of creating a stub keyboard
layout engine if there's no previous layout engine. Avoid this as it
shouldn't be necessary and it creates ordering dependencies between
tests. In particular, running EventConverterEvdevImplTest after
XkbLayoutEngineVkTest causes a crash when we enable use_xkbcommon in GN.
This shows up as a flaky failure on infra:

  flaky failures [ignored]:
  EventConverterEvdevImplTest.KeyPress

Removing the implicit initialization required fixes to a few components:

- KeyEvent tests must create a layout engine if they call GetDomKey or GetCharacter
- ozone/x11 needs to provide a layout engine to chromium. There was no
  layout engine support in that component at all. This needs followup
  work as Linux builds that need proper keyboard layout support need to
  use XkbKeyboardLayoutEngine rather than StubKeyboardLayoutEngine.
- Chromedriver needs to create a layout engine if run headlessly in X11
  (USE_X11) builds without an X display. This is a particularly unusual
  case as we've building ozone code in a non-ozone build.

This patch makes those components create a StubKeyboardLayoutEngine
explicitly, which is equivalent to the previous implicit behavior.

KeyEvent tests that rely on the global layout (because they call
GetDomKey or GetCharacter) now use ScopedKeyboardLayout, which has been
updated to create a StubKeyboardLayoutEngine under the hood. If more
control is required, a custom layout engine can be provided via
ScopedKeyboardLayoutEngine (ozone specific).

Bug: 1026406

Change-Id: I925ed2d0de880e19eaee41a2b36595fea467b6bb
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1928250
Reviewed-by: Scott Violet <sky@chromium.org>
Reviewed-by: Kevin Schoedel <kpschoedel@chromium.org>
Reviewed-by: John Chen <johnchen@chromium.org>
Commit-Queue: Michael Spang <spang@chromium.org>

keycode_text_conversion_ozone.cc
@@ -11,8 +11,11 @@
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/events/keycodes/keyboard_code_conversion.h"
-#include "ui/events/ozone/layout/keyboard_layout_engine.h"
+#include "ui/events/ozone/layout/stub/stub_keyboard_layout_engine.h"
+
+#if defined(USE_OZONE)
 #include "ui/events/ozone/layout/keyboard_layout_engine_manager.h"
+#endif
 
 #if defined(USE_X11)
 bool ConvertKeyCodeToTextOzone
@@ -23,8 +26,13 @@ bool ConvertKeyCodeToText
      int modifiers,
      std::string* text,
      std::string* error_msg) {
+#if defined(USE_OZONE)
   ui::KeyboardLayoutEngine* keyboard_layout_engine =
       ui::KeyboardLayoutEngineManager::GetKeyboardLayoutEngine();
+#else
+  auto keyboard_layout_engine =
+      std::make_unique<ui::StubKeyboardLayoutEngine>();
+#endif
   ui::DomCode dom_code = ui::UsLayoutKeyboardCodeToDomCode(key_code);
   int event_flags = ui::EF_NONE;
 

key_converter_unittest.cc
@@ -101,6 +101,7 @@ void CheckShiftChar(ui::KeyboardCode key_code, char character, char lower) {
 }  // namespace
 
 TEST(KeyConverter, SingleChar) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   KeyEventBuilder builder;
   std::list<KeyEvent> key_events;
   builder.SetText("h", "h")->SetKeyCode(ui::VKEY_H)->Generate(&key_events);
@@ -108,6 +109,7 @@ TEST(KeyConverter, SingleChar) {
 }
 
 TEST(KeyConverter, SingleNumber) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   KeyEventBuilder builder;
   std::list<KeyEvent> key_events;
   builder.SetText("1", "1")->SetKeyCode(ui::VKEY_1)->Generate(&key_events);
@@ -115,6 +117,7 @@ TEST(KeyConverter, SingleNumber) {
 }
 
 TEST(KeyConverter, MultipleChars) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   KeyEventBuilder builder;
   std::list<KeyEvent> key_events;
   builder.SetText("h", "h")->SetKeyCode(ui::VKEY_H)->Generate(&key_events);
@@ -124,6 +127,7 @@ TEST(KeyConverter, MultipleChars) {
 }
 
 TEST(KeyConverter, WebDriverSpecialChar) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   KeyEventBuilder builder;
   std::list<KeyEvent> key_events;
   builder.SetKeyCode(ui::VKEY_SPACE)->SetText(" ", " ")->Generate(&key_events);
@@ -142,6 +146,7 @@ TEST(KeyConverter, WebDriverSpecialNonCharKey) {
 }
 
 TEST(KeyConverter, FrenchKeyOnEnglishLayout) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   KeyEventBuilder builder;
   std::string e_acute = base::WideToUTF8(L"\u00E9");
   std::list<KeyEvent> key_events;
@@ -175,6 +180,7 @@ TEST(KeyConverter, NeedsCtrlAndAlt) {
 #endif
 
 TEST(KeyConverter, UppercaseCharDoesShift) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   KeyEventBuilder shift_builder;
   shift_builder.SetKeyCode(ui::VKEY_SHIFT);
   KeyEventBuilder a_builder;
@@ -189,6 +195,7 @@ TEST(KeyConverter, UppercaseCharDoesShift) {
 }
 
 TEST(KeyConverter, UppercaseSymbolCharDoesShift) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   KeyEventBuilder shift_builder;
   shift_builder.SetKeyCode(ui::VKEY_SHIFT);
   KeyEventBuilder one_builder;
@@ -203,6 +210,7 @@ TEST(KeyConverter, UppercaseSymbolCharDoesShift) {
 }
 
 TEST(KeyConverter, UppercaseCharUsesShiftOnlyIfNecessary) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   std::list<KeyEvent> key_events;
   KeyEventBuilder shift_builder;
   key_events.push_back(shift_builder.SetType(kRawKeyDownEventType)
@@ -262,6 +270,7 @@ TEST(KeyConverter, ToggleModifiers) {
 }
 
 TEST(KeyConverter, AllShorthandKeys) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   KeyEventBuilder builder;
   std::list<KeyEvent> key_events;
   builder.SetKeyCode(ui::VKEY_RETURN)
@@ -319,6 +328,7 @@ TEST(KeyConverter, MAYBE_AllEnglishKeyboardSymbols) {
 }
 
 TEST(KeyConverter, AllEnglishKeyboardTextChars) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
   std::string kLowerChars = "0123456789abcdefghijklmnopqrstuvwxyz";
   std::string kUpperChars = ")!@#$%^&*(ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   for (size_t i = 0; i < kLowerChars.length(); ++i) {

keycode_text_conversion_unittest.cc
@@ -32,11 +32,13 @@ void CheckCharToKeyCode(char character, ui::KeyboardCode key_code,
                        key_code, modifiers);
 }
 
+#if defined(OS_WIN)
 void CheckCharToKeyCode(wchar_t character, ui::KeyboardCode key_code,
                         int modifiers) {
   CheckCharToKeyCode16(base::WideToUTF16(std::wstring(1, character))[0],
                        key_code, modifiers);
 }
+#endif
 
 void CheckCantConvertChar(wchar_t character) {
   std::wstring character_string;
@@ -67,6 +69,8 @@ std::string ConvertKeyCodeToTextNoError(ui::KeyboardCode key_code,
 #endif
 
 TEST(KeycodeTextConversionTest, MAYBE_KeyCodeToText) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
+
   EXPECT_EQ("a", ConvertKeyCodeToTextNoError(ui::VKEY_A, 0));
   EXPECT_EQ("A",
       ConvertKeyCodeToTextNoError(ui::VKEY_A, kShiftKeyModifierMask));
@@ -100,6 +104,8 @@ TEST(KeycodeTextConversionTest, MAYBE_KeyCodeToText) {
 #endif
 
 TEST(KeycodeTextConversionTest, MAYBE_CharToKeyCode) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_ENGLISH_US);
+
   CheckCharToKeyCode('a', ui::VKEY_A, 0);
   CheckCharToKeyCode('A', ui::VKEY_A, kShiftKeyModifierMask);
 
@@ -116,35 +122,15 @@ TEST(KeycodeTextConversionTest, MAYBE_CharToKeyCode) {
   CheckCantConvertChar(L'\u2159');
 }
 
-#if (defined(OS_LINUX) && !defined(OS_CHROMEOS)) || defined(OS_MACOSX)
-// Not implemented on Linux.
-// Fails if German layout is not installed on Mac.
-#define MAYBE_NonShiftModifiers DISABLED_NonShiftModifiers
-#else
-#define MAYBE_NonShiftModifiers NonShiftModifiers
-#endif
-
-TEST(KeycodeTextConversionTest, MAYBE_NonShiftModifiers) {
-  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_GERMAN);
 #if defined(OS_WIN)
+TEST(KeycodeTextConversionTest, NonShiftModifiers) {
+  ui::ScopedKeyboardLayout keyboard_layout(ui::KEYBOARD_LAYOUT_GERMAN);
   int ctrl_and_alt = kControlKeyModifierMask | kAltKeyModifierMask;
   CheckCharToKeyCode('@', ui::VKEY_Q, ctrl_and_alt);
   EXPECT_EQ("@", ConvertKeyCodeToTextNoError(ui::VKEY_Q, ctrl_and_alt));
-#elif defined(OS_MACOSX)
-  EXPECT_EQ("@", ConvertKeyCodeToTextNoError(
-      ui::VKEY_L, kAltKeyModifierMask));
-#endif
 }
 
-#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_MACOSX)
-// Not implemented on Linux.
-// Fails if tested layouts are not installed on Mac.
-#define MAYBE_NonEnglish DISABLED_NonEnglish
-#else
-#define MAYBE_NonEnglish NonEnglish
-#endif
-
-TEST(KeycodeTextConversionTest, MAYBE_NonEnglish) {
+TEST(KeycodeTextConversionTest, NonEnglish) {
   // For Greek and Russian keyboard layouts, which are very different from
   // QWERTY, Windows just uses virtual key codes that match the QWERTY layout,
   // and translates them to other characters.  If we wanted to test something
@@ -165,3 +151,4 @@ TEST(KeycodeTextConversionTest, MAYBE_NonEnglish) {
               ConvertKeyCodeToTextNoError(ui::VKEY_B, 0));
   }
 }
+#endif

commit 7d29051f5b9fe6d6de2bfb421d46c7b3539ea266

chromedriver: Include <memory> for std::make_unique().
IWYU. Follow-up to commit c9f2cbb69 ("events: ozone: Remove implicit init
from GetKeyboardLayoutEngine"), which broke the non-libc++ non-ozone build:

../../chrome/test/chromedriver/keycode_text_conversion_ozone.cc:34:12: error: no member named 'make_unique' in namespace 'std'
      std::make_unique<ui::StubKeyboardLayoutEngine>();
      ~~~~~^
../../chrome/test/chromedriver/keycode_text_conversion_ozone.cc:34:52: error: expected '(' for function-style cast or type construction
      std::make_unique<ui::StubKeyboardLayoutEngine>();
                       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
../../chrome/test/chromedriver/keycode_text_conversion_ozone.cc:34:54: error: expected expression
      std::make_unique<ui::StubKeyboardLayoutEngine>();
                                                     ^

Bug: 957519
Change-Id: I54d132875ffc65495cde651d40b1ea87b5b0b284
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1945176
Auto-Submit: Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
Commit-Queue: John Chen <johnchen@chromium.org>
Reviewed-by: John Chen <johnchen@chromium.org>
Cr-Commit-Position: refs/heads/master@{#720517}

keycode_text_conversion_ozone.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversion_utils.h"
 #include "base/strings/utf_string_conversions.h"