d03144c43e4af1ad3493f21aa2397e2289a06ab2
Tricia Crichton <triciac@chromium.org>
2020-02-11 16:22:06 +0000

    [ChromeDriver] After test timeout, trigger crash
    After a test times out during a ChromeDriver command, trigger a Chrome
    crash and a minidump collection for troubleshooting. This is not
    available on tests connecting to a remote browser, or when running on
    Android.
    
    
    Bug: 1045241
    Change-Id: I3cb82d95875d0f58a4acee69d249a92801b8024b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2032238
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Tricia Crichton <triciac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#740311}

--- client/chromedriver.py
+++ client/chromedriver.py
@@ -139,6 +139,7 @@ class ChromeDriver(object):
           ChromeDriver.retried_tests.append(kwargs.get('test_name'))
           self._InternalInit(*args, **kwargs)
         else:
+          self._RequestCrash()
           raise
 
   def _InternalInit(self, server_url, chrome_binary=None, android_package=None,
@@ -276,6 +277,8 @@ class ChromeDriver(object):
       self.w3c_compliant = True
       self._session_id = response['value']['sessionId']
       self.capabilities = self._UnwrapValue(response['value']['capabilities'])
+      self.debuggerAddress = str(
+          self.capabilities['goog:chromeOptions']['debuggerAddress'])
     elif isinstance(response['status'], int):
       self.w3c_compliant = False
       self._session_id = response['sessionId']
@@ -322,7 +325,13 @@ class ChromeDriver(object):
 
   def _ExecuteCommand(self, command, params={}):
     params = self._WrapValue(params)
-    response = self._executor.Execute(command, params)
+    try:
+      response = self._executor.Execute(command, params)
+    except Exception as e:
+      if e.message.startswith('timed out') and self._session_id != None:
+        self._RequestCrash()
+      raise e
+
     if ('status' in response
         and response['status'] != 0):
       raise _ExceptionForLegacyResponse(response)
@@ -330,6 +339,14 @@ class ChromeDriver(object):
           and 'error' in response['value']):
       raise _ExceptionForStandardResponse(response)
     return response
+
+  def _RequestCrash(self):
+    tempDriver = ChromeDriver(self._server_url,
+      debugger_address=self.debuggerAddress, test_name='_forceCrash')
+    tempDriver.SendCommandAndGetResult("Page.crash", {})
+    # allow time to complete writing the minidump
+    time.sleep(5)
+    tempDriver.Quit();
 
   def ExecuteCommand(self, command, params={}):
     params['sessionId'] = self._session_id
