e4a183dca0a71e1d4e2f0b3b47b74b59aab20b94
Scott Violet <sky@chromium.org>
2020-02-14 17:36:59 +0000

    chromedriver: makes chrome driver write out exe name for weblayer
    Prior to this the executable name was an empty string. This is problematic
    as the executable name is written to a file followed by the command line
    args. If the executable name is empty, it means the first argument is
    treated as the executable name, which effectively means what should be
    the first argument is not seen as an argument to weblayer.
    
    BUG=1052173
    TEST=covered by wpt
    
    Change-Id: Icac6504e7174582a1744d23079b193b6da6a40e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2057085
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#741481}

--- chrome/device_manager.cc
+++ chrome/device_manager.cc
@@ -82,6 +82,11 @@ Status Device::SetUp(const std::string& package,
     use_debug_flag = true;
   } else if (package.find("weblayer") != std::string::npos) {
     command_line_file = "/data/local/tmp/weblayer-command-line";
+    // This name isn't really important, what is important is that it's
+    // non-empty. If empty, it means weblayer treats the the first value of
+    // |args| as the executable name, and not an argument (in other words,
+    // args[0] is effectively ignored as a command line switch).
+    known_exec_name = "weblayer_shell";
   }
 
   if (!use_running_app) {
