8d73aa57d62899147a8c60a921170acd1a5bc1f7
Hans Wennborg <hans@chromium.org>
2020-06-18 18:02:32 +0000

    Include-what-you-use related to logging.h (chrome)
    Add direct includes for things provided transitively by logging.h
    (or by other headers including logging.h).
    
    This is in preparation for cleaning up unnecessary includes of
    logging.h in header files (so if something depends on logging.h,
    it needs include it explicitly), and for when logging.h no longer
    includes check.h, check_op.h, and notreached.h.
    
    Bug: 1031540
    Change-Id: I892ab5d05fe410447fa61541a32285dcb535db0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2249798
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#779903}

--- window_commands.cc
+++ window_commands.cc
@@ -11,6 +11,7 @@
 #include <utility>
 
 #include "base/callback.h"
+#include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
