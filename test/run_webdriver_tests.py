web-platform-tests/wpt

commit cd82725023b0bbde750858397134bbdc0df1fd36

Pass server config to WebDriver via a file instead of an env variable. (#28834)

tools/serve/test_serve.py
@@ -1,4 +1,3 @@
-import json
 import os
 import pickle
 import platform
@@ -78,15 +77,9 @@ def test_pickle():
         pickle.dumps(c)
 
 
-def test_config_json_length():
-    # we serialize the config as JSON for pytestrunner and put it in an env
-    # variable, which on Windows must have a length <= 0x7FFF (int16)
-    with ConfigBuilder() as c:
-        data = json.dumps(c.as_dict_for_wd_env_variable())
-    assert len(data) <= 0x7FFF
-
 def test_alternate_host_unspecified():
     ConfigBuilder(browser_host="web-platform.test")
+
 
 @pytest.mark.parametrize("primary, alternate", [
     ("web-platform.test", "web-platform.test"),

tools/wptrunner/wptrunner/executors/pytestrunner/runner.py
@@ -43,17 +43,22 @@ def run(path, server_config, session_config, timeout=0, environ=None):
 
     old_environ = os.environ.copy()
     try:
-        os.environ["WD_HOST"] = session_config["host"]
-        os.environ["WD_PORT"] = str(session_config["port"])
-        os.environ["WD_CAPABILITIES"] = json.dumps(session_config["capabilities"])
-        os.environ["WD_SERVER_CONFIG"] = json.dumps(server_config.as_dict_for_wd_env_variable())
-        if environ:
-            os.environ.update(environ)
-
-        harness = HarnessResultRecorder()
-        subtests = SubtestResultRecorder()
-
         with TemporaryDirectory() as cache:
+            os.environ["WD_HOST"] = session_config["host"]
+            os.environ["WD_PORT"] = str(session_config["port"])
+            os.environ["WD_CAPABILITIES"] = json.dumps(session_config["capabilities"])
+
+            config_path = os.path.join(cache, "wd_server_config.json")
+            os.environ["WD_SERVER_CONFIG_FILE"] = config_path
+            with open(config_path, "w") as f:
+                json.dump(server_config.as_dict(), f)
+
+            if environ:
+                os.environ.update(environ)
+
+            harness = HarnessResultRecorder()
+            subtests = SubtestResultRecorder()
+
             try:
                 pytest.main(["--strict",  # turn warnings into errors
                              "-vv",  # show each individual subtest and full failure logs

tools/wptserve/wptserve/config.py
@@ -68,31 +68,6 @@ class Config(Mapping):
     def as_dict(self):
         return json_types(self.__dict__)
 
-    # Environment variables are limited in size so we need to prune the most egregious contributors
-    # to size, the origin policy subdomains.
-    def as_dict_for_wd_env_variable(self):
-        result = self.as_dict()
-
-        for key in [
-            ("subdomains",),
-            ("domains", "alt"),
-            ("domains", ""),
-            ("all_domains", "alt"),
-            ("all_domains", ""),
-            ("domains_set",),
-            ("all_domains_set",)
-        ]:
-            target = result
-            for part in key[:-1]:
-                target = target[part]
-            value = target[key[-1]]
-            if isinstance(value, dict):
-                target[key[-1]] = {k:v for (k,v) in value.items() if not k.startswith("op")}
-            else:
-                target[key[-1]] = [x for x in value if not x.startswith("op")]
-
-        return result
-
 
 def json_types(obj):
     if isinstance(obj, dict):

webdriver/tests/support/fixtures.py
@@ -105,7 +105,8 @@ def http(configuration):
 
 @pytest.fixture
 def server_config():
-    return json.loads(os.environ.get("WD_SERVER_CONFIG"))
+    with open(os.environ.get("WD_SERVER_CONFIG_FILE"), "r") as f:
+        return json.load(f)
 
 
 @pytest.fixture(scope="session")

bayandin/chromedriver

commit 1daecbfdda22ee94cfff2837617f0186a052cc6f

Pass server config via a file.
This change unblocks importer, where webdriver tests are failing due to changes in https://github.com/web-platform-tests/wpt/pull/28834

Change-Id: Ia07ed877aa808da41a49260900a390c87e9c5521
No-Export: true
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2890259
Reviewed-by: Jason Chase <chasej@chromium.org>
Reviewed-by: John Chen <johnchen@chromium.org>
Commit-Queue: Kyle Ju <kyleju@chromium.org>
Cr-Commit-Position: refs/heads/master@{#882198}

test/run_webdriver_tests.py
@@ -194,11 +194,13 @@ def set_up_config(path_finder, chromedriver_server):
       }
   })
 
+  config_path = os.path.join(tempfile.mkdtemp(), "wd_server_config.json")
+  os.environ["WD_SERVER_CONFIG_FILE"] = config_path
   # Port numbers are defined at
   # https://cs.chromium.org/chromium/src/third_party/blink/tools
   # /blinkpy/web_tests/servers/wptserve.py?l=23&rcl=375b34c6ba64
   # 5d00c1413e4c6106c7bb74581c85
-  os.environ["WD_SERVER_CONFIG"] = json.dumps({
+  config_dict = {
     "doc_root": path_finder.path_from_chromium_base(CHROMIUM_WPT_DIR),
     "browser_host": "web-platform.test",
     "domains": {"": {"": "web-platform.test",
@@ -206,7 +208,10 @@ def set_up_config(path_finder, chromedriver_server):
                      "www.www": "www.www.web-platform.test",
                      "www1": "www1.web-platform.test",
                      "www2": "www2.web-platform.test"}},
-    "ports": {"ws": [9001], "wss": [9444], "http": [8001], "https": [8444]}})
+    "ports": {"ws": [9001], "wss": [9444], "http": [8001], "https": [8444]}}
+  with open(config_path, "w") as f:
+    json.dump(config_dict, f)
+
 
 def run_test(path, path_finder, port, skipped_tests=[]):
   abs_path = os.path.abspath(path)